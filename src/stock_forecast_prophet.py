da43f16d47fc474bf06f59a1cc28ab93313112f8
Jessica Andersson <anderssonjessica92@gmail.com>
2017-12-07 21:05:13 +0100

    Validation

--- src/stock_forecast_prophet.py
+++ src/stock_forecast_prophet.py
@@ -3,6 +3,7 @@ from fbprophet import Prophet
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import median_absolute_error, mean_absolute_error
 
 def load_data(filename):
     return pd.read_csv(filename, delimiter=';', decimal=',')
@@ -22,6 +23,7 @@ def train_model_prophet(df):
 def make_prediction(model, dates_to_predict):
     return model.predict(dates_to_predict)
 
+
 def plot_prophet_prediction(forecast, filename):
     fig = model.plot(forecast)
     fig.savefig('../plots/stock_forecast_plt_{}.png'.format(filename))
@@ -31,14 +33,30 @@ def plot_prophet_prediction(forecast, filename):
     plt.close(fig_comp)
 
 
+def validation(stock_data, column_to_predict):
+    split_number = int(stock_data.shape[0]*0.2)
+    training_set = stock_data.loc[split_number:, :]
+    validation_set = prepare_data_for_prophet(stock_data.loc[:split_number, :], column_to_predict)
+    model = train_model_prophet(prepare_data_for_prophet(training_set, column_to_predict))
+    dates_to_predict = pd.DataFrame(validation_set['ds'], columns=['ds'])#pd.to_datetime(validation_set['ds'], format='%Y-%m-%d')
+    validation_set['prediction'] = make_prediction(model, dates_to_predict)['yhat']
+    median_ae = median_absolute_error(validation_set['y'], validation_set['prediction'])
+    mean_ae = mean_absolute_error(validation_set['y'], validation_set['prediction'])
+    print('Median {}, mean {}'.format(median_ae, mean_ae))
+    print(validation_set.head(20))
+
+
 if __name__ == '__main__':
     #filename = 'OMX30-2013-09-22-2017-09-22.csv' #os.getenv('FILENAME')
-    #filename = 'INVE-B-2013-09-22-2017-09-22.csv'
-    filename = 'HM-B-2000-09-22-2017-09-22.csv'
+    filename = 'INVE-B-2013-09-22-2017-12-06.csv'
+    #filename = 'HM-B-2000-09-22-2017-09-22.csv'
     column_to_predict = 'Closing price'
     stock_data = load_data('../data/{}'.format(filename))
     stock_data[column_to_predict] = stock_data[column_to_predict].apply(np.log)
+
+    validation(stock_data, column_to_predict)
+
     model = train_model_prophet(prepare_data_for_prophet(stock_data, column_to_predict))
-    dates_to_predict = model.make_future_dataframe(periods=1095)
+    dates_to_predict = model.make_future_dataframe(periods=730)
     prediction = make_prediction(model, dates_to_predict)
     plot_prophet_prediction(prediction, filename)
